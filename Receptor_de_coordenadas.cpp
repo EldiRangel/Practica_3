8b97bbea17bde572ba428f061bce3a79fb06b642
EldiRangel <eldi456.rangel@gmail.com>
2024-11-17 08:06:49 -0800

    fin del void para determinar el cuadrante

--- Receptor_de_coordenadas.cpp
+++ Receptor_de_coordenadas.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 void Cuadrante(double x, double y){
-
+if (x>0&&y>0){
+    cout<<"El punto ("<<x<<", "<<y<<") ubicado en el primer cuadrante."<<endl;
+    } else if (x<0&&y>0){
+    cout<<"El punto ("<<x<<", "<<y<<") ubicado en el segundo cuadrante."<<endl;
+    } else if (x<0&&y<0){
+    cout<<"El punto ("<<x<<", "<<y<<") ubicado en el tercer cuadrante."<<endl;
+    } else if (x>0&&y<0){
+    cout<<"El punto ("<<x<<", "<<y<<") ubicado en el cuarto cuadrante."<<endl;
+    } else if (x==0&&y==0){
+    cout<<"El punto ("<<x<<", "<<y<<") ubicado en el origen."<<endl;
+    } else if (x==0){
+    cout<<"El punto ("<<x<<", "<<y<<") ubicado en el eje Y."<<endl;
+    } else if (y==0){
+     cout<<"El punto ("<<x<<", "<<y<<") ubicado en el eje X."<<endl;
+    }
 
 
 
